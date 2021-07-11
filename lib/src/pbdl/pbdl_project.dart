abbaa2ebe1ac4132c91615cb2b7fa0a4146f29b7
Ivan <42812006+ivan-015@users.noreply.github.com>
2021-07-11 14:41:18 -0500

    Made factory methods static so we can await them

--- lib/src/pbdl/pbdl_project.dart
+++ lib/src/pbdl/pbdl_project.dart
@@ -1,10 +1,10 @@
 // maybe we dont need this one anymore, or we might use it for PBDL
 
 import 'package:pbdl/src/pbdl/abstract_design_node_factory.dart';
-import 'package:pbdl/src/pbdl/pbdl_node.dart';
 import 'package:pbdl/src/pbdl/pbdl_page.dart';
 import 'package:pbdl/src/input/sketch/entities/style/shared_style.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:pbdl/src/util/sketch/sac_installer.dart';
 
 part 'pbdl_project.g.dart';
 
@@ -33,13 +33,13 @@ class PBDLProject implements PBDLNodeFactory {
   Map<String, dynamic> toJson() => _$PBDLProjectToJson(this);
 
   /// Method that creates and returns a [PBDLProject] from a Sketch file `path`
-  factory PBDLProject.fromSketch(String path) {
-    
+  static Future<PBDLProject> fromSketch(String path) async {
+    await SACInstaller.installAndRun();
     return PBDLProject();
   }
 
   /// Method that creates and returns a [PBDLProject] from figma `projectID` and `key`
-  factory PBDLProject.fromFigma(String projectID, String key) {
+  static Future<PBDLProject> fromFigma(String projectID, String key) async {
     return PBDLProject();
   }
 }
