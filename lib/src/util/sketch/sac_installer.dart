0bc5a495924cf425ce256c45063455c47a5978af
Ivan <42812006+ivan-015@users.noreply.github.com>
2021-07-11 14:34:39 -0500

    Update SACInstaller

--- lib/src/util/sketch/sac_installer.dart
+++ lib/src/util/sketch/sac_installer.dart
@@ -1,19 +1,29 @@
 import 'dart:io';
 import 'package:path/path.dart' as p;
 import 'dart:convert';
+import 'package:quick_log/quick_log.dart';
 
 class SACInstaller {
+  static Logger log = Logger('SACInstaller');
+
   /// Method that installs the SAC submodule and its dependencies.
   ///
   /// Returns the exit code of the process as an `int`.
-  static Future<int> installSAC() async {
+  static Future<int> installAndRun() async {
     var installExitCode = await _installSubmodule();
-    // If `installExitCode` is not successful, return exit code
+    // Returning exit code if there was an error
     if (installExitCode != 0) {
       return installExitCode;
     }
+
+    var installDepExitCode = await _installDependencies();
+    // Returning exit code if there was an error
+    if (installDepExitCode != 0) {
+      return installDepExitCode;
+    }
+
     // Return exit code to caller
-    return await _installDependencies();
+    return await _runSAC();
   }
 
   static Future<int> _installSubmodule() async {
@@ -27,17 +37,35 @@ class SACInstaller {
   }
 
   static Future<int> _installDependencies() async {
-    // TODO: Find a way to check if SAC did not run correctly.
-    var process = await Process.start('npm', ['run', 'prod'],
-        workingDirectory:
-            p.join(Directory.current.path, 'SketchAssetConverter'));
+    var pathToScript =
+        p.join(Directory.current.path, 'tool', 'install_sac_dependencies.sh');
+    var install = await Process.start(pathToScript, []);
+
+    await stdout.addStream(install.stdout);
+    await stderr.addStream(install.stderr);
+    return await install.exitCode;
+  }
+
+  static Future<int> _runSAC() async {
+    var process = await Process.start(
+      'npm',
+      ['run', 'prod'],
+      workingDirectory: p.join(Directory.current.path, 'SketchAssetConverter'),
+    );
+    var exitCode = 1;
 
     await for (var event in process.stdout.transform(utf8.decoder)) {
       if (event.toLowerCase().contains('server is listening on port')) {
-        return 0;
+        exitCode = 0;
+        log.fine('SAC started successfully');
+        break;
+      } else if (event.toLowerCase().contains('error')) {
+        exitCode = 1;
+        log.error(event);
+        break;
       }
     }
     process?.kill();
-    return 0;
+    return exitCode;
   }
 }
--- tool/install_sac_dependencies.sh
+++ tool/install_sac_dependencies.sh
@@ -37,7 +37,6 @@ if [[ -z "${SAC_ENDPOINT}" ]]; then
   start_spin "Installing Sketch Asset Converter and its dependencies"
   tput civis # stop mouse pointer from blinking
   cd ./SketchAssetConverter && npm i # Assuming install.sh is being run from Parabeac-Core directory
-  pub get
   stop_spin
   tput cnorm # return mouse pointer
   echo -e "\033[1;32m[====]\033[0m Installed Sketch Asset Converter dependencies"
