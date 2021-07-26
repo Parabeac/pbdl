d5d1d5e922c6fb9a70410d3c9e2bb1d22f28f296
Ivan <42812006+ivan-015@users.noreply.github.com>
2021-07-26 18:24:06 -0500

    Update boolean operation to return PBDL Image

--- lib/src/input/figma/entities/layers/boolean_operation.dart
+++ lib/src/input/figma/entities/layers/boolean_operation.dart
@@ -1,6 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:pbdl/src/input/figma/helper/figma_asset_processor.dart';
-import 'package:pbdl/src/pbdl/pbdl_boolean_operation.dart';
+import 'package:pbdl/src/pbdl/pbdl_image.dart';
 import 'package:pbdl/src/pbdl/pbdl_node.dart';
 import 'package:quick_log/quick_log.dart';
 import '../abstract_figma_node_factory.dart';
@@ -59,16 +59,14 @@ class BooleanOperation extends FigmaVector implements FigmaNodeFactory {
   @override
   Future<PBDLNode> interpretNode() {
     imageReference = FigmaAssetProcessor().processImage(UUID);
-    return Future.value(PBDLBooleanOperation(
-      children: children,
-      booleanOperation: booleanOperation,
-      type: type,
-      style: style,
-      boundaryRectangle: boundaryRectangle?.interpretFrame(),
+    return Future.value(PBDLImage(
       UUID: UUID,
+      boundaryRectangle: boundaryRectangle?.interpretFrame(),
+      isVisible: isVisible,
+      name: name,
+      style: style?.interpretStyle(),
+      booleanOperation: booleanOperation,
       prototypeNodeUUID: transitionNodeID,
-      transitionDuration: transitionDuration,
-      transitionEasing: transitionEasing,
       imageReference: imageReference,
     ));
   }
