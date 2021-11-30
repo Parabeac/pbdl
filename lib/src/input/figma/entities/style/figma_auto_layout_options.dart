0b2ecab9c46567fbe06a4f704245b443dd8c5e8c
Bryan Figueroa <bryan@parabeac.com>
2021-11-29 20:50:30 -0700

    FigmaAutoLayoutOptions to PBDL

--- lib/src/input/figma/entities/style/figma_auto_layout_options.dart
+++ lib/src/input/figma/entities/style/figma_auto_layout_options.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:pbdl/src/pbdl/pbdl_auto_layout_options.dart';
 
 part 'figma_auto_layout_options.g.dart';
 
@@ -30,6 +31,53 @@ class FigmaAutoLayoutOptions {
     this.primaryAxisAlignItems,
     this.counterAxisAlignItems,
   });
+
+  PBDLAutoLayoutOptions interpretOptions() {
+    var orientation = layoutMode == 'HORIZONTAL'
+        ? Orientation.HORIZONTAL
+        : Orientation.VERTICAL;
+
+    var primaryAxisSizing = _getAxisSizing(primaryAxisSizingMode);
+    var counterAxisSizing = _getAxisSizing(counterAxisSizingMode);
+
+    var primaryAlignment = _getAlignment(primaryAxisAlignItems);
+    var counterAlignment = _getAlignment(counterAxisAlignItems);
+
+    return PBDLAutoLayoutOptions(
+      orientation: orientation,
+      spacing: itemSpacing,
+      leftPadding: paddingLeft,
+      rightPadding: paddingRight,
+      topPadding: paddingTop,
+      bottomPadding: paddingBottom,
+      primaryAxisSizing: primaryAxisSizing,
+      counterAxisSizing: counterAxisSizing,
+      primaryAxisAlignment: primaryAlignment,
+      counterAxisAlignment: counterAlignment,
+    );
+  }
+
+  AxisAlignment _getAlignment(String axisAlignment) {
+    switch (axisAlignment) {
+      case 'CENTER':
+        return AxisAlignment.CENTER;
+      case 'MAX':
+        return AxisAlignment.END;
+      case 'SPACE_BETWEEN':
+        return AxisAlignment.SPACE_BETWEEN;
+      default:
+        return AxisAlignment.START;
+    }
+  }
+
+  AxisMode _getAxisSizing(String sizingMode) {
+    switch (sizingMode) {
+      case 'FIXED':
+        return AxisMode.FIXED;
+      default:
+        return AxisMode.HUGGED;
+    }
+  }
 
   Map<String, dynamic> toJson() => _$FigmaAutoLayoutOptionsToJson(this);
 
