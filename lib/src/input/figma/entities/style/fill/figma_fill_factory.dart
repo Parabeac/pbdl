c5a939b1006ac0436f417bc512897ec2f8868bca
Bryan Figueroa <bryan@parabeac.com>
2022-01-19 19:17:06 -0700

    Set up fill factory

---
+++ lib/src/input/figma/entities/style/fill/figma_fill_factory.dart
@@ -0,0 +1,22 @@
+import 'package:pbdl/src/input/figma/entities/style/figma_fill.dart';
+import 'package:pbdl/src/input/figma/entities/style/fill/gradient_fill_type.dart';
+
+import 'fill_type.dart';
+
+class FigmaFillFactory {
+  static final _fills = <FigmaFill>[
+    SolidFillType(),
+    GradientFillType(),
+    ImageFillType(),
+  ];
+  static FigmaFill getFill(Map<String, dynamic> json) {
+    for (var fill in _fills) {
+      if (fill.type == json['type']) {
+        return fill.createFigmaFill(json);
+      } else if (json['type'].toLowerCase().contains('gradient')) {
+        return GradientFillType.fromJson(json);
+      }
+    }
+    return null;
+  }
+}
