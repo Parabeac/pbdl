139e267e68b8dbf740f172b515adccb9d6bbadfa
Bryan Figueroa <bryan@parabeac.com>
2023-02-17 17:57:25 -0700

    Renamed material for material 2

--- lib/src/pbdl/global_styles/design_systems/material/material2_design.dart
+++ lib/src/pbdl/global_styles/design_systems/material/material2_design.dart
@@ -1,36 +1,21 @@
 import 'package:pbdl/src/pbdl/global_styles/design_systems/design_system_theme_data.dart';
 import 'package:pbdl/src/pbdl/global_styles/design_systems/material/material_color_scheme.dart';
 
-class MaterialDesign extends DesignSystemThemeData {
+class MaterialDesign2 extends DesignSystemThemeData {
   static const _textStyles = [
-    'bodyLarge',
-    'bodyMedium',
-    'bodySmall',
     'bodyText1',
     'bodyText2',
     'button',
     'caption',
-    'displayLarge',
-    'displayMedium',
-    'displaySmall',
     'headline1',
     'headline2',
     'headline3',
     'headline4',
     'headline5',
     'headline6',
-    'headlineLarge',
-    'headlineMedium',
-    'headlineSmall',
-    'labelLarge',
-    'labelMedium',
-    'labelSmall',
     'overline',
     'subtitle1',
     'subtitle2',
-    'titleLarge',
-    'titleMedium',
-    'titleSmall',
   ];
 
   static final _colorSchemes = [
@@ -48,9 +33,9 @@ class MaterialDesign extends DesignSystemThemeData {
 
   int get totalMaterialCount => _textStyles.length + supportedAttributesCount;
 
-  MaterialDesign()
+  MaterialDesign2()
       : super(
-          'material',
+          'material2',
           textStyles: _textStyles,
           colorSchemes: _colorSchemes,
         );
